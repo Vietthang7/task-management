9be258c3eead060dfb8b74a828fea8b0ac5b6c3a
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-08-25 16:07:03 +0700

    11. Tính năng đăng ký

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,4 +1,6 @@
 const taskRoute = require("./task.route");
+const userRoute = require("./user.route");
 module.exports.index = (app) => {
   app.use("/tasks", taskRoute);
+  app.use("/users", userRoute);
 }
