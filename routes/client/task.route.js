24e9855e46e6d4d4f41c9b75598b49974a803f5c
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-08-25 15:35:04 +0700

    Xóa 1 hoặc nhiều công việc

--- routes/client/task.route.js
+++ routes/client/task.route.js
@@ -7,4 +7,5 @@ router.get("/detail/:id", controller.detail);
 router.patch("/change-status", controller.changeStatus);
 router.post("/create", controller.create);
 router.patch("/edit/:id", controller.edit);
+router.patch("/delete", controller.delete);
 module.exports = router;
