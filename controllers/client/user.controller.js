36d06a272fae4cba403f630d01eebe1bdefa3c10
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-08-29 00:09:28 +0700

    1. Tính năng đăng nhập

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -11,7 +11,7 @@ module.exports.register = async (req, res) => {
       deleted: false
     });
 
-    if(existUser) {
+    if (existUser) {
       res.json({
         code: 400,
         message: "Email đã tồn tại!"
@@ -41,4 +41,33 @@ module.exports.register = async (req, res) => {
       message: "Not Found"
     });
   }
+}
+// [POST] /users/login
+module.exports.login = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = await User.findOne({
+    email: email,
+    deleted: false
+  });
+  if (!user) {
+    res.json({
+      code: 400,
+      message: "Email không tồn tại!"
+    });
+    return;
+  }
+  if (md5(password) != user.password) {
+    res.json({
+      code: 400,
+      message: "Sai mật khẩu!"
+    });
+    return;
+  }
+  res.json({
+    code: 200,
+    message: "Đăng nhập thành công",
+    token: user.token
+  });
 }
